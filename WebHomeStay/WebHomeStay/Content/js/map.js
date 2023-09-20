db9fc5a6e55f4fcc9bd783d8378882d2619a99a0
Nhựt Phi <phi211102@gmail.com>
2023-09-20 23:55:17 +0700

    upload contact

---
+++ WebHomeStay/WebHomeStay/Content/js/map.js
@@ -0,0 +1,18 @@
+﻿// Khởi tạo bản đồ
+function initMap() {
+    // Vị trí ban đầu
+    const initialPosition = { lat: 37.7749, lng: -122.4194 };
+
+    // Tạo bản đồ và đặt vị trí ban đầu
+    const map = new google.maps.Map(document.getElementById('map'), {
+        center: initialPosition,
+        zoom: 12 // Mức độ zoom mặc định
+    });
+
+    // Đặt đánh dấu tại vị trí ban đầu
+    const marker = new google.maps.Marker({
+        position: initialPosition,
+        map: map,
+        title: 'San Francisco, CA'
+    });
+}
